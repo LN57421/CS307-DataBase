3f94a49a512cdc580fb7d3d1ec435b960a825d81
LN <96339601+LN57421@users.noreply.github.com>
2023-04-11 19:02:14 +0800

    Update table_1.sql

--- table_1.sql
+++ table_1.sql
@@ -8,71 +8,76 @@
 -- 1. 作者表
 CREATE TABLE authors (
     author_id SERIAL PRIMARY KEY,
-    author_name VARCHAR(255) NOT NULL,
+    author_name TEXT NOT NULL,
     registration_time TIMESTAMP NOT NULL,
-    phone VARCHAR(20) UNIQUE NOT NULL
+    phone TEXT UNIQUE NOT NULL
 );
 
 -- 2. 文章表
 CREATE TABLE posts (
     post_id SERIAL PRIMARY KEY,
-    author_id INTEGER NOT NULL REFERENCES authors (author_id),
-    title VARCHAR(255) NOT NULL,
+    author_id INTEGER NOT NULL REFERENCES authors(author_id),
+    title TEXT NOT NULL,
     content TEXT NOT NULL,
     posting_time TIMESTAMP NOT NULL,
-    posting_city VARCHAR(255) NOT NULL
+    posting_city TEXT NOT NULL
 );
 
 -- 3. 评论表
 CREATE TABLE replies (
     reply_id SERIAL PRIMARY KEY,
-    content TEXT NOT NULL,
-    stars INTEGER NOT NULL,
-    author_id INTEGER NOT NULL REFERENCES authors (author_id),
-    post_id INTEGER NOT NULL REFERENCES posts (post_id)
+    reply_content TEXT NOT NULL,
+    reply_stars INTEGER NOT NULL,
+    reply_author INTEGER NOT NULL REFERENCES authors(author_id),
+    post_id INTEGER NOT NULL REFERENCES posts(post_id)
 );
 
 -- 4. 次级评论表
 CREATE TABLE secondary_replies (
     secondary_reply_id SERIAL PRIMARY KEY,
-    content TEXT NOT NULL,
-    stars INTEGER NOT NULL,
-    author_id INTEGER NOT NULL REFERENCES authors (author_id),
-    reply_id INTEGER NOT NULL REFERENCES replies (reply_id)
+    secondary_reply_content TEXT NOT NULL,
+    secondary_reply_stars INTEGER NOT NULL,
+    secondary_reply_author INTEGER NOT NULL REFERENCES authors(author_id),
+    reply_id INTEGER NOT NULL REFERENCES replies(reply_id)
 );
 
--- 5. 关注表
-CREATE TABLE followed_authors (
-    author_id INTEGER PRIMARY KEY REFERENCES authors (author_id),
-    follower_author_id INTEGER NOT NULL
-);
-
--- 6. 收藏表
-CREATE TABLE favorited_posts (
-    post_id INTEGER PRIMARY KEY REFERENCES posts (post_id),
-    favorited_author_id INTEGER NOT NULL
-);
-
--- 7. 分享表
-CREATE TABLE shared_posts (
-    post_id INTEGER PRIMARY KEY REFERENCES posts (post_id),
-    sharing_author_id INTEGER NOT NULL
-);
-
--- 8. 点赞表
-CREATE TABLE liked_posts (
-    post_id INTEGER PRIMARY KEY REFERENCES posts (post_id),
-    liking_author_id INTEGER NOT NULL
-);
-
--- 9. 分类表
+-- 5. 分类表
 CREATE TABLE categories (
     category_id SERIAL PRIMARY KEY,
-    category_name VARCHAR(255) NOT NULL
+    category_name TEXT NOT NULL
+);
+
+-- 6. 关注表
+CREATE TABLE followed_authors (
+    author_id INTEGER NOT NULL REFERENCES authors(author_id),
+    follower_author_id INTEGER NOT NULL REFERENCES authors(author_id),
+    PRIMARY KEY (author_id, follower_author_id)
+);
+
+-- 7. 收藏表
+CREATE TABLE favorited_posts (
+    post_id INTEGER NOT NULL REFERENCES posts(post_id),
+    favorited_author_id INTEGER NOT NULL REFERENCES authors(author_id),
+    PRIMARY KEY (post_id, favorited_author_id)
+);
+
+-- 8. 分享表
+CREATE TABLE shared_posts (
+    post_id INTEGER NOT NULL REFERENCES posts(post_id),
+    sharing_author_id INTEGER NOT NULL REFERENCES authors(author_id),
+    PRIMARY KEY (post_id, sharing_author_id)
+);
+
+-- 9. 点赞表
+CREATE TABLE liked_posts (
+    post_id INTEGER NOT NULL REFERENCES posts(post_id),
+    liking_author_id INTEGER NOT NULL REFERENCES authors(author_id),
+    PRIMARY KEY (post_id, liking_author_id)
 );
 
 -- 10. 文章分类表
 CREATE TABLE post_categories (
-    post_id INTEGER PRIMARY KEY REFERENCES posts (post_id),
-    category_id INTEGER NOT NULL REFERENCES categories (category_id)
+    post_id INTEGER NOT NULL REFERENCES posts(post_id),
+    category_id INTEGER NOT NULL REFERENCES categories(category_id),
+    PRIMARY KEY (post_id, category_id)
 );
