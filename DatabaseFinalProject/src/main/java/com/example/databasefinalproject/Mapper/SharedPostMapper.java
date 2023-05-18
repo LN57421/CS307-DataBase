963efb4f41e579a998eafc81d33afffccd00ac7f
LN <12110416@mail.sustech.edu.cn>
2023-05-18 19:29:43 +0800

    19.28 添加部分Mapper实现

---
+++ DatabaseFinalProject/src/main/java/com/example/databasefinalproject/Mapper/SharedPostMapper.java
@@ -0,0 +1,25 @@
+package com.example.databasefinalproject.Mapper;
+
+import com.example.databasefinalproject.Entity.SharedPost;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface SharedPostMapper {
+    @Select("select * from shared_posts where author_id = #{authorId} and post_id = #{postId}")
+    SharedPost findRelationByAuthorIdAndPostId(String authorId, String postId);
+
+    @Insert("insert into shared_posts(author_id, post_id) values(#{authorId}, #{postId})")
+    void createRelation(String authorId, String postId);
+
+    @Delete("delete from shared_posts where author_id = #{authorId} and post_id = #{postId}")
+    void deleteRelation(String authorId, String postId);
+
+    @Select("SELECT * FROM shared_posts WHERE author_id = #{authorId}")
+    List<SharedPost> findSharedPostsByAuthorId(String authorId);
+
+}
